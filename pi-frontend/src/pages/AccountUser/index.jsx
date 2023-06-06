fed03e832c200f560ab5d552d581fe81899b4447
Vinícius Rotondo <vinirs.trade@gmail.com>
2023-06-05 23:51:02 -0300

    Adicionando jsx e css nas pages

---
+++ pi-frontend/src/pages/AccountUser/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import "./style.css";
+
+const accountUser = () => {
+    return(
+        <>
+            <Header />
+
+            <Footer />
+        </>
+    );
+};
+
+export default accountUser;

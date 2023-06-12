16c3fc7c2bdd5c5c6e8f4d9732552c07312b02af
Julio Nunes <julio.nt753@gmail.com>
2023-06-12 19:39:00 -0300

    carousel

--- pi-frontend/src/pages/Home/components/carousel.jsx
+++ pi-frontend/src/pages/Home/components/carousel.jsx
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import banner from "../../../images/banner goró 1.png"
-import banner1 from "../../../images/banner topo 1.png"
-import banner2 from "../../../images/slide-indicator.png"
+import banner from '../../../images/banner goró 1.png';
+import banner1 from '../../../images/banner topo 1.png';
+import banner2 from '../../../images/slide-indicator.png';
 
 const DemoCarousel = () => {
-        return (
-            <Carousel showThumbs = {false}>
-                <div>
-                    <img src= {banner} />
-                </div>
-                <div>
-                    <img src={banner1} />
-                </div>
-                <div>
-                    <img src={banner2}/>
-                </div>
-            </Carousel>
-        );
-    
+	return (
+		<Carousel
+			showThumbs={false}
+			dynamicHeight={false}
+			autoPlay={true}
+			showStatus={false}
+		>
+			<div>
+				<img src={banner} style={{ height: '540px' }} />
+			</div>
+			<div>
+				<img src={banner1} style={{ height: '540px' }} />
+			</div>
+			<div>
+				<img src={banner2} style={{ height: '540px' }} />
+			</div>
+		</Carousel>
+	);
 };
 
-export default DemoCarousel
-
+export default DemoCarousel;
--- pi-frontend/src/pages/Home/index.jsx
+++ pi-frontend/src/pages/Home/index.jsx
@@ -10,64 +10,61 @@ import api from '../../services/api';
 import DemoCarousel from './components/carousel';
 
 const Home = () => {
-	const [products, setProducts] = useState([])
+	const [products, setProducts] = useState([]);
 
 	useEffect(() => {
-		loadProducts()
-	}, [])
+		loadProducts();
+	}, []);
 
 	const loadProducts = async () => {
-		const response = await api.get('product')
-		setProducts(response.data)
-
-	}
-
+		const response = await api.get('product');
+		setProducts(response.data);
+	};
 	return (
 		<>
 			<Header />
 			<DemoCarousel />
-		{/* 	<Slide banner={banner} type='Acessórios' title='Goro Style' /> */}
+			{/* 	<Slide banner={banner} type='Acessórios' title='Goro Style' /> */}
 			<section className='produtos'>
 				<h3>TODOS OS PRODUTOS</h3>
 				<div className='alignProdutos'>
 					<ul className='produtos__container'>
-
-
- 						{products.map((product) => ( 
-						<li className="produto__container">
-							<Link
-								to={"/product"}
-								className="link__produto"
-								state={{ id: product.id }}
-							>
-								<div className="produto__img">
-									<img
-										className="foto"
-										src={`http://localhost:3000/img/${product.image}`}
-										alt="banner"
-									/>
-									<div className="icone">
-										<a href="#"
-											><img
-												className="icone"
-												src="http://localhost:3000/img/sacola.png"
-												alt="banner"
-										/></a>
+						{products.map(product => (
+							<li className='produto__container'>
+								<Link
+									to={'/product'}
+									className='link__produto'
+									state={{ id: product.id }}
+								>
+									<div className='produto__img'>
+										<img
+											className='foto'
+											src={`http://localhost:3000/img/${product.image}`}
+											alt='banner'
+										/>
+										<div className='icone'>
+											<a href='#'>
+												<img
+													className='icone'
+													src='http://localhost:3000/img/sacola.png'
+													alt='banner'
+												/>
+											</a>
+										</div>
 									</div>
-								</div>
-								<div className="produto__conteudo">
-									<p className="produto__nome">
-										 {product.name }
-									</p>
-									<p className="produto__descricao">
-										{product.type }
-									</p>
-									<p className="produto__valor">
-										R$  {product.price}
-									</p>
-								</div>
-							</Link>
-						</li>
+									<div className='produto__conteudo'>
+										<p className='produto__nome'>
+											{product.name}
+										</p>
+										<p className='produto__descricao'>
+											{product.type}
+										</p>
+										<p className='produto__valor'>
+											R$ {product.price}
+										</p>
+									</div>
+								</Link>
+							</li>
 						))}
 					</ul>
 				</div>
